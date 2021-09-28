3b74794a98f0dd5e947c4e4f024153a7e73f30b1
Junga8 <thapajunga@gmail.com>
2021-09-28 12:09:33 -0400

    this branch throws errrow with unity

--- TaskManagementSystem/App_Start/WebApiConfig.cs
+++ TaskManagementSystem/App_Start/WebApiConfig.cs
@@ -5,6 +5,9 @@ using System.Net.Http;
 using System.Web.Http;
 using Microsoft.Owin.Security.OAuth;
 using Newtonsoft.Json.Serialization;
+using Service;
+using Unity;
+
 
 namespace TaskManagementSystem
 {
@@ -12,11 +15,15 @@ namespace TaskManagementSystem
     {
         public static void Register(HttpConfiguration config)
         {
+
+
+          
+
             // Web API configuration and services
             // Configure Web API to use only bearer token authentication.
             config.SuppressDefaultHostAuthentication();
             config.Filters.Add(new HostAuthenticationFilter(OAuthDefaults.AuthenticationType));
-
+          
             // Web API routes
             config.MapHttpAttributeRoutes();
 
---
+++ TaskManagementSystem/Controllers/QuoteController.cs
@@ -0,0 +1,58 @@
+﻿using DataAccess;
+using Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace TaskManagementSystem.Controllers
+{
+    public class QuoteController : ApiController
+    {
+        private readonly IQuoteService _quoteService; 
+
+
+        public QuoteController()
+        {
+
+        }
+      
+      
+
+        //construction injection 
+        public QuoteController(IQuoteService quoteService)
+        {
+            _quoteService = quoteService; 
+                 
+
+        }
+        // GET: api/Quote
+        public IEnumerable<Quote> Get()
+        {
+            return _quoteService.GetAllTasks().ToList();
+        }
+
+        // GET: api/Quote/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Quote
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/Quote/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Quote/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
